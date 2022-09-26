f935a5ceff5fdd86ce8985316aed2d33240e5901
AnnaSLRomero <asromero@ualberta.ca>
2022-09-25 23:52:23 -0600

    assignment3 python

---
+++ Assignment3/Assignment3_practice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 17:21:23 2022
+
+@author: annaromero
+"""
+
+##variable ops
+sub_code = "sub"
+subnr_int = 2
+subnr_str = "2"
+
+print(sub_code + subnr_str)
+print(sub_code + subnr_int)
+
+print(sub_code + " " + subnr_str)
+
+print(sub_code + " " + subnr_str*3)
+
+print((sub_code + subnr_str)*3)
+
+print((sub_code*3)+(subnr_str*3))
+
+#list ops
